7caf409649e498e025e93461cee3ca43eb8ae384
Andrew Delapaz <ardelapaz@yahoo.com>
2018-01-18 17:47:15 -0500

    Routing Assignment

---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+    <section className= "Album">
+        This is where the album will eventually be shown
+    </section>
+);
+
+export default Album;
